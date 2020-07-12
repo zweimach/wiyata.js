1bf762493d3cc071be272382b94b1e9a1d4d61fa
Ananda Umamil <zweimach@zweimach.org>
2020-07-12 11:13:21 +0700

    Merge config files to package.json

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  collectCoverageFrom: ["<rootDir>/src/**/*.js"],
-  testMatch: ["<rootDir>/src/**/?(*.)(spec|test).js"],
-  moduleDirectories: ["node_modules"]
-};
